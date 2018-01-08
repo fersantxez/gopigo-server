8183aab4733976defa3bd88d75e4cd701be76cbe
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2018-01-08 21:32:32 +0000

    run.sh

--- run.sh
+++ run.sh
@@ -19,12 +19,5 @@ export FLASK_APP=gopigo-server.py
 #export FLASK_DEBUG=1
 export FLASK_DEBUG=0
 
-<<<<<<< HEAD
-=======
-#not needed on a virtual environment#
-# for other systems
-# sudo pip install -r requirements.txt
->>>>>>> 4744e6622f0e324f5e329ab2ff38d580fe87fa0f
-
 #run
 flask run --with-threads --host='0.0.0.0'
