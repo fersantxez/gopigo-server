a90dfbdf1b61038e8e546fa00bb08bf93ee152c8
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2018-01-08 21:29:54 +0000

    add venv to run.sh

--- run.sh
+++ run.sh
@@ -3,17 +3,21 @@
 # gopigo-server: a Flask-based REST server to control a GoPiGo Robot
 # Author: Fernando Sanchez (fer@groundcontrol.me)
 
-#dev: activate virtual environment
-source venv/bin/activate
+export VENV_DIR=${PWD}"/venv"
+
+if [ ! -d "$VENV_DIR" ]; then
+	virtualenv $VENV_DIR
+fi
+#not needed on a virtual environment#
+# for other systems
+source ${VNENV_DIR}/bin/activate
+sudo pip install -r requirements.txt
+
 
 #point Flask to the Main app
 export FLASK_APP=gopigo-server.py
 export FLASK_DEBUG=0
 
-
-#not needed on a virtual environment#
-# for other systems
-# sudo pip install -r requirements.txt
 
 #run
 flask run --with-threads --host='0.0.0.0'
