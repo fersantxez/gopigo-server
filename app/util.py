b8d0bbce177cf9cfb086d638c45e4981b1f6b9e1
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2018-02-09 23:51:17 +0000

    wip for fixe bucket name to mac address

--- app/util.py
+++ app/util.py
@@ -3,10 +3,12 @@ import datetime
 import os
 import io #to write JPG to file as string
 import logging
+import fcntl, socket, struct
 
 from app import db
 from config import Config
 from .models import Document
+import gcp
 
 import picamera, camera
 
@@ -24,6 +26,11 @@ def get_default_iface_name_linux():
             except:
                 continue
 
+def get_iface_mac_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
+    return ':'.join(['%02x' % ord(char) for char in info[18:24]])
+
 def sound(spk):
     #Calls the Espeak TTS Engine to read aloud a sentence
     # This function is going to read aloud some text over the speakers
@@ -39,42 +46,48 @@ def sound(spk):
     print cmd_beg+spk+cmd_end
     call ([cmd_beg+spk+cmd_end], shell=True)
 
-def filename_from_date():
+def filename_from_date( file_type, extension ):
     """Generate a filename for a media file based on the current time"""
     date_string = str(datetime.datetime.now())
-    date_string = 'image'+date_string+'.jpg'
+    date_string = file_type+'-'+date_string+'.'+extension
     date_string = date_string.replace(":", "")  # Strip out the colon from date time.
     date_string = date_string.replace(" ", "")  # Strip out the space from date time.
 
-    return date_string    
+    return date_string
 
 def create_document_from_file( path, type, user_id ):
     """create an object with all the metadata from a file.
     Uploads it to the database as a full object"""
 
+    logger.debug('Creating document from file {}'.format(path))
     try:
         with open(path, 'rb') as input_file:
+            #store the file in GCS
+            logger.debug('file {} opened'.format(path))
+            filename = os.path.basename(path)
+            gcp.upload_file_to_bucket( input_file )
             document = Document(
-                name=os.path.basename(path),   #keep extension: useful to look for it statically
+                name=filename,   #keep extension: useful to look for it statically
                                                         #otherwise remove with:
                                                         #os.path.splitext(filename)[0],
                 type=type,
                 extension=os.path.splitext(path)[1],
                 size=os.path.getsize(path),
                 user_id=user_id,  
-                location=path,
-                body=input_file.read()
+                location=gcp.create_uri_from_name( filename )   #store the object's URI instead of the file path,
+                #,body=None     #body is empty in DB and stored in GCS, instead of storing the full doc with: 
+                #input_file.read()
                 )
         input_file.close()
         db.session.add(document)
         db.session.commit()
-        #os.remove(document.location)   #this breaks creating in bulk, plus doesnt really make sense
+        if not path == Config.EMPTY_PICTURE: 
+            os.remove(path)   #FIXME: emove the file from local disk
         return document
 
     except Exception as exc:
         db.session.rollback()
-        logger.error('ERROR uploading document {}: {}'.format(path, str(exc)), 'error')
-
+        logger.error('ERROR uploading document {}: {}'.format(path, str(exc)))
 
 def take_photo():
     #Takes a picture from the camera
@@ -95,7 +108,7 @@ def take_photo_from_last_frame(camera):
     """Creates a picture document with the last frame received in the camera"""
     frame = camera.get_frame()
     #save frame to file for temp storage and display
-    file_location = os.path.join(Config.MEDIA_FOLDER, filename_from_date())
+    file_location = os.path.join(Config.MEDIA_FOLDER, filename_from_date( 'picture', 'jpg'))
     logger.debug('Writing file to {}'.format(file_location))
     with open (file_location, 'wb') as file:
         file.write(frame)
